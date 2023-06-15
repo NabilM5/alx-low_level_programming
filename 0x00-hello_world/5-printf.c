85be3ff36fd7250511199c66d72748f19e875796
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-06-15 23:33:14 +0000

    Update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function includes a description of the return value.
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
