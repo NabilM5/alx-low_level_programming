e680012d69610b1e8cefeb6c1384687539dd89bf
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-06-15 23:16:25 +0000

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+* main - function includes a description of the return value.
+*
+* Return: 0 on successful execution
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
