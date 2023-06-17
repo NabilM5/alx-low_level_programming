9959983ef8113431b12acfab885b12f1c474ada9
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-06-17 02:43:06 +0000

    Update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - The entry point of the program
+  *
+  * Return: returns 0 to indicate successful execution
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
