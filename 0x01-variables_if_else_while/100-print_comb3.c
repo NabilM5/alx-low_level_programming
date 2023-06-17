09fefdc206eadfc49ef847c101c0abedb26d6e21
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-06-17 03:01:40 +0000

    Update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  *
+  * return: to 0 successful
+  */
+int main(void)
+{
+	int n1 = 0, n2;
+	while (n1 <= 9)
+	{
+		n2 = 0;
+		while (n2 <= 9)
+		{
+			if (n1 != n2 && n1 < n2)
+			{
+				putchar (n1 + 48);
+				putchar (n2 + 48);
+
+				if (n1 + n2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			n2++;
+		}
+		n1++;
+	}
+	putchar('\n');
+	return (0);
+}
