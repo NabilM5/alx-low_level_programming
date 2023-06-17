d14b95937aaf9f5b8b7387d46e0c29a15aecb22d
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-06-17 01:35:30 +0000

    Update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - The entry point of the program
+  *
+  * Return: 0 successful execution
+  */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
