1b5deb939044e6c9c16b6c2e2c0aa4b633c6e23a
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-06-20 20:51:39 +0000

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - is the entry point of the program
- * 
- * Description: 
+ * main- is the entry point of the program
+ *
+ * Description: print _putchar to stdout
  *
  * Return: to 0 successful
  */
-int main<void>
+int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
-
