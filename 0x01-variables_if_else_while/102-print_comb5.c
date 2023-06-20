560bd284e861481b2552dd177989c298a8a6bb08
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-06-20 00:46:03 +0000

    Update

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+* main - function
+*
+* Description:  prints all possible combinations of two two-digit numbers
+*
+* Return: to 0 success
+*/
+int main(void)
+{
+	int num1 = 0, num2;
+
+	while (num1 <= 99)
+	{
+		num2 = num1;
+		while (num2 <= 99)
+		{
+			if (num2 != num1)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
