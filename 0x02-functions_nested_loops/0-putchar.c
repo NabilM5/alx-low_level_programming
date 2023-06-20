b70e06d7b22f3836996d94328465ebaf3857652e
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-06-20 18:52:30 +0000

    Update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry point of the program
+ * 
+ * Description: 
+ *
+ * Return: to 0 successful
+ */
+int main<void>
+{
+	printf("_putchar\n");
+	return (0);
+}
+
