efc78466b08dbc840f3c3d63ab10d489f9b145a6
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-06-21 00:50:43 +0000

    Update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - print lowercase character
+ *
+ * Return: to 1 successful
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return(1);
+	return(0);
+}
