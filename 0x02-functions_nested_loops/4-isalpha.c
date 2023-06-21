8d4ac37c9cc2a64a8107a101f16613d3e28240e9
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-06-21 01:27:26 +0000

    Update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,7 @@
  *
  * Return: 1 if character is letter, print lowercase or uppercase
  */
- int _isalpha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
