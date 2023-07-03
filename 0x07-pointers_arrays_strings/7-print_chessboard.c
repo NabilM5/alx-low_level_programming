21af4b27de6615df959ea9b2637581a34c058ba8
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-07-03 19:48:17 +0000

    Update

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints a chessboard.
+ *
+ * @a: The chessboard to be printed.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i1, i2;
+
+	for (i1 = 0; a[i1][7]; i1++)
+	{
+		for (i2 = 0; i2 < 8; i2++)
+			_putchar(a[i1][i2]);
+
+		_putchar('\n');
+	}
+}
