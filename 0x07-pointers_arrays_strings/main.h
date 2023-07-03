5434175e1a9b89f03fa4461aa150e3c1cceb2bb9
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-07-03 17:24:58 +0000

    Update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 
-void *_memset(void *s, int b, size_t n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -12,6 +12,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
+int _putchar(char c);
 #endif /* MAIN_H */
 
