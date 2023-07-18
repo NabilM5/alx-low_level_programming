295d743f0584efe119e794d59324578ffd54a2d5
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-07-18 00:48:30 +0000

    Update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,18 +53,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (!name || age < 0 || !owner)
 		return (NULL);
-	
+
 	dog = (dog_t *)  malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	
+
 	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if ((*dog).name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	
+
 	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if ((*dog).owner == NULL)
 	{
@@ -76,6 +76,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = _strcopy(dog->name, name);
 	dog->age = age;
 	dog->owner = _strcopy(dog->owner, owner);
-	
+
 	return (dog);
 }
