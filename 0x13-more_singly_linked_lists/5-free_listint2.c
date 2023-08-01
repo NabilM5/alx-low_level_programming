7f69b794117aea401b269c51f769420316e0604c
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-08-01 08:54:27 -0700

    Update

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+/*
+ * File: 5-free_nodeint2.c
+ * Auth: Nabil Mouhamech
+ */
+
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a linkint_t list and sets the head to NULL
+ * @head: Pointer to the first node in the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
