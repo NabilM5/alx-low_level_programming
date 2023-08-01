f62fb80f83c618d958d55e06284e161d232b75e2
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-08-01 06:14:37 -0700

    Update

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-print_listint.c
+ * Auth: Nabil Mouhamech
+ */
+
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the listint_t list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}
