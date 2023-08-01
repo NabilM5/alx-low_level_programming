f5d1df0efec64260bcc9c012261526a614a610ec
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-08-01 09:27:49 -0700

    Update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,11 +14,15 @@ void free_listint2(listint_t **head)
 	listint_t *current = *head;
 	listint_t *next;
 
+	if (head == NULL)
+		return;
+
 	while (current != NULL)
 	{
 		next = current->next;
 		free(current);
 		current = next;
 	}
+	
 	*head = NULL;
 }
