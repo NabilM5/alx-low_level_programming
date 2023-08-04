f71bb784c7afe7e682a06cd66f3bba70ead6122d
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-08-03 17:25:24 -0700

    Update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,23 +14,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int base = 1, result = 0, len = 0;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len];)
+	while (b[len])
 		len++;
 
-	for (len -= 1; len >= 0; len--)
+	while (len)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[len - 1] != '0' && b[len - 1] != '1')
 			return (0);
 
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		if (b[len - 1] == '1')
+			result += base;
+		base *= 2;
+		len--;
 	}
-
-	return (num);
+	return (result);
 }
