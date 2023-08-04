efb4eb60f2a2fe4a1f7ec6c864d0d21fc177b77b
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-08-03 17:49:03 -0700

    Update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,11 +13,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
-
-	max <<= index;
-	if (max == 0)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	*n |= max;
+
+	*n ^= (1 << index);
+
 	return (1);
 }
