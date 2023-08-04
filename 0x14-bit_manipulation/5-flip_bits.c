31675c4ab540204927c8e324d9969440ac95c1ef
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-08-03 17:59:52 -0700

    Update

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+/*
+ * File: 5-flip_bits.c
+ * Auth: Naibl Mouhamech
+ */
+#include "main.h"
+
+/**
+ * flip_bits - A function that gets number of bits needed to flip
+ * @n: How many bit flips are needed to equal m for n
+ * @m: The number to set other equal
+ *
+ * Return: The number of fliped bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m, bits = 0;
+
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
+
+	return (bits);
+}
