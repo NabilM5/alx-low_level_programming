b2dc826bc71e4b057ab0f294c300013fd6cf42b9
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-08-07 18:31:22 -0700

    Update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,94 +6,42 @@
 #include "main.h"
 
 /**
- * safe_close - A function that closes a file and prints error when closed file
- * @description: Description error for closed file
- *
- * Return: 1 on success, -1 on failure
- */
-int safe_close(int description)
-{
-	int error;
-
-	error = close(description);
-	if (error < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", description);
-	return (error);
-}
-
-/**
- * open_file - Opens a file and returns its file descriptor
- * @filename: The name of the file to open
- * @flags: The flags to use when opening the file
- *
- * Return: The file descriptor on success, exit on failure
- */
-int open_file(const char *filename, int flags)
-{
-	int fd = open(filename, flags);
-
-	if (fd < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
-		safe_close(fd);
-		exit(98);
-	}
-	return (fd);
-}
-
-/**
- * read_write_loop - Reads from one file and write to another
- * @from_fd: The file descriptor to read from
- * @to_fd: The file descriptor to write to
- */
-void read_write_loop(int from_fd, int to_fd)
-{
-	char buffer[1024];
-	int bytes_read, error;
-
-	while ((bytes_read = read(from_fd, buffer, sizeof(buffer))) > 0)
-	{
-		error = write(to_fd, buffer, bytes_read);
-		if (error < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
-			safe_close(from_fd);
-			safe_close(to_fd);
-			exit(99);
-		}
-	}
-	if (bytes_read < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
-		safe_close(from_fd);
-		safe_close(to_fd);
-		exit(98);
-	}
-}
-
-/**
  * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * @ac: The number of arguments supplied to the program.
+ * @av: An array of pointers to the arguments.
  *
  * Return: 1 on success.
  */
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
-	int from_fd, to_fd;
+	int fd_from, fd_to, rd_stat, wr_stat;
+	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
+	char buffer[BUFSIZ];
 
-	if (argc != 3)
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	rd_stat = 1;
+	while (rd_stat)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		rd_stat = read(fd_from, buffer, BUFSIZ);
+		if (rd_stat == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		if (rd_stat > 0)
+		{
+			wr_stat = write(fd_to, buffer, rd_stat);
+			if (wr_stat != rd_stat || wr_stat == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		}
 	}
-
-	from_fd = open(argv[1], O_RDONLY);
-	to_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-
-	read_write_loop(from_fd, to_fd);
-
-	safe_close(from_fd);
-	safe_close(to_fd);
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }
