3e7c5f125937ff1a4d36fcc82b54ab90d827cb85
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-09-21 17:27:07 -0700

    update

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,24 +1,5 @@
-int add(int a, int b)
-{
-  return (a + b);
-}
-
-int sub(int a, int b)
-{
-  return (a - b);
-}
-
-int div(int a, int b)
-{
-  return (a / b);
-}
-
-int mul(int a, int b)
-{
-  return (a * b);
-}
-
-int mod(int a, int b)
-{
-  return (a % b);
-}
+int add(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
